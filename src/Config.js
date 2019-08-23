5d7a533fcbcb074e965aaf809a04292e14609950
Laureen Cruz <laureencrz@outlook.com>
2019-08-23 15:05:05 -0700

    changes package.json

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 export default {
-    API_FOLDERS: 'https://nameless-temple-32242.herokuapp.com/api/folders',
-    API_NOTES:'https://nameless-temple-32242.herokuapp.com/api/notes',
+    API_FOLDERS: 'https://morning-shelf-22585.herokuapp.com/',
     API_KEY: process.env.REACT_APP_API_KEY,
   }
