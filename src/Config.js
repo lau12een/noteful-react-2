ba5f6cb5d0433fce6ddc1537dd87e0f6b9101357
Laureen Cruz <laureencrz@outlook.com>
2019-08-23 15:12:49 -0700

    changed endpoints

--- src/Config.js
+++ src/Config.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 export default {
-    API_FOLDERS: 'https://morning-shelf-22585.herokuapp.com/',
+    API_FOLDERS: 'https://morning-shelf-22585.herokuapp.com/api/folders',
+    API_NOTES:'https://morning-shelf-22585.herokuapp.com/api/folders',
+    API_ENDPOINT: 'https://morning-shelf-22585.herokuapp.com/',
     API_KEY: process.env.REACT_APP_API_KEY,
   }
